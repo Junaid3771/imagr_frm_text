7be2c6c1107359c804223e2f86550929b5b1f540
junaid . <junaidhackerrank@gmail.com>
2025-05-04 00:20:26 +0530

    first commit

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+#main.py
 from agents import ImageGenAgent
 from PIL import Image
 
@@ -5,7 +6,7 @@ async def chat_response(prompt: str):
     """
     Streams the response (text + image) from the model.
     """
-    api_key = "AIzaSyCc0FzAq-hUkzg2RwB0hASbRDM5E2dVpcQ"
+    api_key = "AIzaSyDlLqbl9RmIM4uOvIWR2-PfJ3r0WbT-bTY"
     agent = ImageGenAgent(api_key=api_key)
 
     response_text, image = agent.generate_image_response(prompt)
